9caf38011ac1c2bfb3ae48be66406e35dc1ea123
seungwoo hyun <p829911@gmail.com>
2023-06-14 12:01:18 +0900

    chapter6: 영속성 어댑터 구현하기

---
+++ src/main/java/io/reflectoring/buckpal/account/adapter/out/persistence/SpringDataAccountRepository.java
@@ -0,0 +1,5 @@
+package io.reflectoring.buckpal.account.adapter.out.persistence;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface SpringDataAccountRepository extends JpaRepository<AccountJpaEntity, Long> {}
---
+++ src/main/java/io/reflectoring/buckpal/account/application/service/GetAccountBalanceService.java
@@ -0,0 +1,20 @@
+package io.reflectoring.buckpal.account.application.service;
+
+import io.reflectoring.buckpal.account.application.port.in.GetAccountBalanceQuery;
+import io.reflectoring.buckpal.account.application.port.out.LoadAccountPort;
+import io.reflectoring.buckpal.account.domain.Account.AccountId;
+import io.reflectoring.buckpal.account.domain.Money;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+class GetAccountBalanceService implements GetAccountBalanceQuery {
+
+  private final LoadAccountPort loadAccountPort;
+
+  @Override
+  public Money getAccountBalance(AccountId accountId) {
+    return loadAccountPort.loadAccount(accountId, LocalDateTime.now())
+        .calculateBalance();
+  }
+}
